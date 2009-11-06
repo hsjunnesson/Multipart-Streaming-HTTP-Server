0f17dc9a205938e4193e3ce6b0d98bdc9c14896f
Hans Sjunnesson <hanssjunnesson@corleone.local>
2009-11-06 20:01:43 +0100

    Whitespace & indentation

--- server.rb
+++ server.rb
@@ -25,33 +25,33 @@ HTML_TYPE = "Content-type: text/html" + N
 BOUND = "BOUND"
 
 class Connection
-    def initialize(socket)
-        @s = socket
+  def initialize(socket)
+    @s = socket
+  end
+
+  def onConnect
+    @s << HTTP_OK
+    @s << "Content-type: multipart/x-mixed-replace;boundary=#{BOUND}" << N
+    @s << N
+    @s << "--#{BOUND}" << N
+
+    range = 1..10
+
+    range.each do |count|
+      t = Time.now()
+      @s << HTML_TYPE
+      @s << N
+      @s << "<html><body>#{t}</body></html>" << N
+      @s << "--#{BOUND}"
+      @s << "--" if count == range.last
+      @s << N
+      @s.flush()
+
+      sleep(1)
     end
-    
-    def onConnect
-        @s << HTTP_OK
-        @s << "Content-type: multipart/x-mixed-replace;boundary=#{BOUND}" << N
-        @s << N
-        @s << "--#{BOUND}" << N
-        
-        range = 1..10
 
-        range.each do |count|
-            t = Time.now()
-            @s << HTML_TYPE
-            @s << N
-            @s << "<html><body>#{t}</body></html>" << N
-            @s << "--#{BOUND}"
-            @s << "--" if count == range.last
-            @s << N
-            @s.flush()
-
-            sleep(1)
-        end
-
-        @s.close
-    end
+    @s.close
+  end
 end
 
 server = TCPServer.open(PORT)
@@ -59,15 +59,15 @@ puts("Server open on #{server.addr[1]}")
 trap("PIPE", "IGNORE")
 
 loop do
-    session = server.accept
-    
-    Thread.start do 
-        begin
-            c = Connection.new(session)
-            c.onConnect()
-        rescue => e
-            puts("Error #$! #{x}")
-        end
+  session = server.accept
+
+  Thread.start do
+    begin
+      c = Connection.new(session)
+      c.onConnect()
+    rescue => e
+      puts("Error #$! #{x}")
     end
+  end
 end
 
